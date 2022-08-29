d6b7017225bd8f771c808b208c69e08ee2e97e88
Joey Cos <joeycosentino24@gmail.com>
2022-08-29 12:34:39 -0400

    used hook to grab params from url

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import queryString from 'query-string';
+import { useLocation } from 'react-router';
 import io from 'socket.io-client';
 
 const Chat = () => {
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    const search = useLocation().search;
+
+    useEffect(() => {
+        const name = new URLSearchParams(search).get('name');
+        const room = new URLSearchParams(search).get('room');
+
+        setName(name);
+        setRoom(room);
+    })
+
     return (
         <h1>Chat</h1>
     )
